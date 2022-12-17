7ebb8f0afdb170823b2a095914b21a7a2d613880
Aaron Berman <aberman189@student.fuhsd.org>
2022-12-16 23:12:15 -0800

    Added some blackjack gameplay
    Needs way to reset player points, auto end round if player points > 22
    Same for dealer

--- src/BlackjackLogic.java
+++ src/BlackjackLogic.java
@@ -3,6 +3,13 @@ public class BlackjackLogic{
 	private int dealerSum;
 	private int playerCardCount;
 	private int playerAceCount;
+	
+	public boolean getDealerMove() {
+		if (dealerSum < 17)
+			return true;
+		else
+			return false;
+	}
 	public void proccessCardDealer(int cardValue){
 			dealerSum+=cardValue;
 	}
@@ -10,14 +17,30 @@ public class BlackjackLogic{
 		if (cardValue == 11)
 			playerAceCount++;
 		else {
-			playerSum +=cardValue;
+			playerSum += cardValue;
 			playerCardCount++;
 		}
 	}
 	public int getPlayerSum() {
 		return playerSum;
 	}
+	public int getPlayerAces() {
+		return playerAceCount;
+	}
 	public int getDealerSum() {
 		return playerSum;
+	}
+	public String getCurrentTotal() {
+		return ("Player: " + playerSum + " " + playerAceCount + "A " + "Dealer:" + dealerSum);
+	}
+	public double getWinner() {
+		if (playerSum == 21 && playerCardCount ==2)
+			return 1.5;
+		else if (playerSum == dealerSum)
+			return 0;
+		else if (playerSum>dealerSum)
+			return 1;
+		else 
+			return -1;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,47 @@
-
+import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
+		String decision, newGame;
+		newGame = "y";
+		double point; //temporary storage for how payout ratio to player
+		double profit = 0; //total money gained/lost by player
+		Scanner kBoard = new Scanner(System.in);
 		Deck deck = new Deck();
 		BlackjackLogic logic = new BlackjackLogic();
-		deck.resetShoe(1);
-		Card s = deck.getRandomCard();
-		logic.proccessCardPlayer(s.toCardPoints());
-		int i = logic.getPlayerSum();
-		System.out.println(i);
+		deck.resetShoe(4);
 		
+		while(newGame.contains("y")) {
+			logic.proccessCardDealer(deck.getRandomCard().toCardPoints());
+			logic.proccessCardPlayer(deck.getRandomCard().toCardPoints());
+			logic.proccessCardPlayer(deck.getRandomCard().toCardPoints());
+			System.out.println(logic.getCurrentTotal());
+			System.out.println("(H) hit or (S) stand?");
+			decision = kBoard.next().toLowerCase();
+			while (decision.contains("h") && logic.getDealerSum()+logic.getPlayerAces()<21) {
+				logic.proccessCardPlayer(deck.getRandomCard().toCardPoints());
+				System.out.println(logic.getCurrentTotal());
+				System.out.println("(H) hit or (S) stand?");
+				decision = kBoard.next().toLowerCase();
+			}
+			while (logic.getDealerMove() == true && !decision.contains("h")){
+				logic.proccessCardDealer(deck.getRandomCard().toCardPoints());
+				System.out.println(logic.getCurrentTotal());
+			}
+			point = logic.getWinner();
+			profit += point;
+			if (point == 1.5)
+				System.out.print("Blackjack, Overall gain/loss $" + profit);
+		else if (point ==1)
+				System.out.print("Player Won, Overall gain/loss $" + profit);
+			else if (point ==-1)
+				System.out.print("Dealer Won, Overall gain/loss $" + profit);
+			else
+				System.out.print("Draw, Overall gain/loss $" + profit);
+			System.out.println(" Play Another Round (Y/N)");
+			newGame = kBoard.next().toLowerCase();
+			if (deck.getCardCount()<20)
+				deck.resetShoe(4);
+		}
 	}
 
 }
